6f360edac52216c4660c9c18cc8298b64307e059
Kunal Bhati <kbnikku@gmail.com>
2025-10-01 23:35:55 +0530

    Responsive Header

--- src/components/layouts/UI/Header.tsx
+++ src/components/layouts/UI/Header.tsx
@@ -2,27 +2,27 @@ import { NavLink } from "react-router-dom";
 
 function Header() {
     return (<>
-        <div className="flex justify-between bg-gray-700 py-2 px-[10%]" >
+        <header className="flex flex-col text-center sm:flex-row justify-between items-center bg-gray-700 py-2 px-[10%]" >
             <h1 className="text-2xl">WorldAtlas</h1>
-            <ul className="flex gap-6">
+            <ul className="flex flex-col mt-2 gap-y-2 sm:flex-row gap-6">
                 <li><NavLink to="/"
-                    className={({ isActive }) => isActive ? "text-gray-500" : "hover:text-gray-400"
+                    className={({ isActive }) => isActive ? "text-black" : "hover:text-black"
                     }
                 >Home</NavLink ></li>
                 <li><NavLink to="/about"
-                    className={({ isActive }) => isActive ? "text-gray-500" : "hover:text-gray-400"
+                    className={({ isActive }) => isActive ? "text-black" : "hover:text-black"
                     }
                 >About</NavLink ></li>
                 <li><NavLink to="/country"
-                    className={({ isActive }) => isActive ? "text-gray-500" : "hover:text-gray-400"
+                    className={({ isActive }) => isActive ? "text-black" : "hover:text-black"
                     }
                 >Country</NavLink ></li>
                 <li><NavLink to="/contact"
-                    className={({ isActive }) => isActive ? "text-gray-500" : "hover:text-gray-400"
+                    className={({ isActive }) => isActive ? "text-black" : "hover:text-black"
                     }
                 >Contact</NavLink ></li>
             </ul>
-        </div>
+        </header>
     </>);
 }
 
