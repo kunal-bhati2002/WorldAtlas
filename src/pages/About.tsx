ee80a48bc91891de8bf08b52117f3b31a3bc29b4
Kunal Bhati <kbnikku@gmail.com>
2025-10-03 00:07:59 +0530

    About Page Started

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,32 @@
+import countryFacts from "../api/countryData.json"
+
 function About() {
     return (<>
-        <h1>About</h1>
+        <section className="container mx-auto">
+            <h1 className="text-md text-center my-6">
+                Here are the Intresting Facts
+                <br />
+                we're proud of
+            </h1>
+            <div className="grid grid-cols-3">
+                <ul>
+                    {
+                        countryFacts.map((curCountry) => {
+                            return (
+                                <li key={curCountry.id}>
+                                    <h2>{curCountry.countryName}</h2>
+                                    <p>Capital :
+                                        <span>
+                                            {curCountry.capital}
+                                        </span>
+                                    </p>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
+        </section>
     </>);
 }
 
